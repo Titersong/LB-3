5594b04af21012abf326ad5e7a5f72635cfc4933
Titersong <daniil.g.301205@gmail.com>
2025-11-17 14:37:23 +0200

    added new images and edited code

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-// Генерація списку зображень
 const imageList = document.getElementById("imageList");
 const img = document.getElementById("mainImage");
+const titles = [
+    "Shin Megami Tensei: Persona",
+    "Persona 2: Innocent Sin",
+    "Persona 2: Eternal Punishment",
+    "Persona 3",
+    "Persona 3 FES",
+    "Persona 4",
+    "Persona 4 Golden",
+    "Persona 5",
+    "Persona 5 Royal",
+    "Persona 3 Reload",
+    "Persona 4 Revival"
+];
 
-for (let i = 1; i <= 9; i++) {
+for (let i = 1; i <= 11; i++) {
     let li = document.createElement("li");
-    li.textContent = "Зображення " + i;
+    li.textContent = titles[i - 1];
     li.onclick = () => changeImage(i);
     imageList.appendChild(li);
 }
 
-// Функція зміни зображення
 function changeImage(num) {
     img.src = `images/${num}.png`;
-    img.alt = `Зображення ${num}`;
+    img.alt = titles[num - 1];
 }
 
-// Функція зміни параметрів
 function applySettings() {
     const width = document.getElementById("imgWidth").value;
     const height = document.getElementById("imgHeight").value;
